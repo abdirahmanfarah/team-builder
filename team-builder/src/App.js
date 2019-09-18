203f47938da961c5bdc439418c7e8fcb6b1ad7dd
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2019-09-18 15:05:11 -0700

    had to restart

---
+++ team-builder/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Form from "./components/Form";
+
+function App() {
+  const [member, setMember] = useState([{
+    name: "Bob", 
+    email: "bobsaget@gmail.com",
+    role: "backend"
+  }]);
+
+
+  return (
+    <div className="App">
+      <h1>Members</h1>
+      
+      {/* <Form />  */}
+
+        {member.map((member, index) => {
+
+          return (
+
+            <div className = "member" key ={index}> 
+              <h2>{member.name}</h2>
+              <p>{member.email}</p>
+              <p>{member.role}</p>
+  
+            </div>
+          )
+        })}
+    </div>
+  );
+}
+
+export default App;
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react-dom';
+
+const Form = props => {
+  return <></>
+}
+export default Form;
